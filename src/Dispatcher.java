7820b8376d00b5d43032bf29481e386f9460f8d2
lightGregory <light.partikaf@gmail.com>
2017-07-09 15:54:01 +0300

    add syso

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -7,14 +7,8 @@ public class Dispatcher {
 	
 	 int integerVariable = 10;
 	 char g = '2';
-	 
-	 String first = integerVariable + ""; // конкатенация с пустой строкой
-	 String second = String.valueOf(integerVariable); // вызов статического метода valueOf класса String
-	 String third = Integer.toString(integerVariable); // вызов метода toString класса-обертки
-	 String four  =	String.valueOf(g);
-	System.out.println();
-	String formatString = "We are printing double variable (%f), string ('%s') and integer variable (%d).";
-	System.out.println(String.format(formatString, 2.3, "habr", 10));
+	 System.out.println("and what?");
+
 }
 	public static void  input(char[] c) {
         for(int i=0;i<c.length;i++) {
